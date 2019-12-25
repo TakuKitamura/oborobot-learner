8f768958a5c2d3f7908ea3923093f420617d78a2
TakuKitamura <tikutaku0128@icloud.com>
2019-12-25 11:56:29 +0900

    add random-forest-model

--- learner.py
+++ learner.py
@@ -3,6 +3,14 @@ from urllib.parse import urlparse
 import collections
 import random
 
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.datasets import make_regression
+
+from decimal import Decimal, ROUND_HALF_UP
+
+def rounding(v):
+    return int(Decimal(str(v)).quantize(Decimal('1'), ROUND_HALF_UP))
+
 def get_clean_url(url):
     parsed_url = urlparse(url)
     clean_url = parsed_url.scheme + '://' + parsed_url.netloc + parsed_url.path
@@ -24,21 +32,40 @@ url_count_dict = collections.Counter(url_list)
 predictor_dict = {
     'explanatory':
         {
-            'url': [],
             'url_count': [],
             'user_rating': []
         },
-    'objective_id': []
+    'objective_id': [],
+    'url': [],
 }
 objective_id = 1
 
 for clean_url in url_count_dict:
-    explanatory_obj = predictor_dict['explanatory']
-    explanatory_obj['url'].append(clean_url)
-    explanatory_obj['url_count'].append(url_count_dict[clean_url])
+    explanatory_dict = predictor_dict['explanatory']
+    explanatory_dict['url_count'].append(url_count_dict[clean_url])
+    explanatory_dict['user_rating'].append(url_count_dict[clean_url] * 10)
 
     predictor_dict['objective_id'].append(objective_id)
+    predictor_dict['url'].append(clean_url)
     objective_id += 1
 
+print('predictor_dict:', predictor_dict)
 
-print(predictor_dict)
+X = []
+y = []
+
+for i in range(0, objective_id - 1):
+    explanatory_dict = predictor_dict['explanatory']
+    X.append([explanatory_dict['url_count'][i], explanatory_dict['user_rating'][i]])
+    y.append(predictor_dict['objective_id'][i])
+
+regr = RandomForestRegressor(max_depth=2, random_state=0)
+regr.fit(X, y)
+print('X:', X ,'| Y:', y)
+print('feature_importances_:', regr.feature_importances_)
+predict_value = [[60, 600]]
+predicted_value = regr.predict(predict_value)[0]
+print('predicted_value:', predicted_value)
+predicted_object_id = rounding(predicted_value)
+print('predicted_object_id:', predicted_object_id)
+print('predicted_url:', predictor_dict['url'][predicted_object_id - 1])
